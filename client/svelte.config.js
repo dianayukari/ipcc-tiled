6e87662429587f8fec05a9f3f368848327255163
dianayukari <yukaridiana@gmail.com>
2025-12-17 21:09:35 +0100

    feat: update svelte configuration and add layout server settings

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,12 +1,20 @@
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { 
-    kit: { 
+const config = {
+	    kit: { 
+        appDir: 'app',
         adapter: adapter({
             pages: 'build',
             assets: 'build',
-            fallback: null
-        }) } };
+            fallback: null,
+            precompress: false,
+            strict: true
+        }),
+        paths: {
+            base: process.env.NODE_ENV === 'production' ? '/ipcctiled' : ''
+        }
+    } 
+};
 
 export default config;
